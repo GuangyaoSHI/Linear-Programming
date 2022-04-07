eb6f750e1d2b7f3a970d71aaa26218bd0ce32081
GuangyaoSHI <33379281+GuangyaoSHI@users.noreply.github.com>
2022-04-07 13:58:00 -0400

    update files

---
+++ 2-LP-CMDP-toy-example-gradient_descent.py
@@ -0,0 +1,347 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  6 17:04:26 2022
+
+@author: gyshi
+"""
+
+import numpy as np
+import networkx as nx
+import random
+import matplotlib.pyplot as plt
+import gurobipy as gp
+from gurobipy import GRB
+from utils import *
+import pickle
+import logging
+import copy
+import time
+import os
+
+
+state_f = ('sf')
+goal = 'sg'
+state_l = ('sl')
+state_init = ('s0')
+
+threshold = 0.3
+print("threshold is {}".format(threshold))
+# generate state transition function
+
+P_s_a = {}
+P_s_a['s0'] = {}
+P_s_a['s0']['a1'] = {}
+P_s_a['s0']['a1']['sg'] = 0.8
+P_s_a['s0']['a1']['sf'] = 0.2
+P_s_a['s0']['a2'] = {}
+P_s_a['s0']['a2']['sg'] = 0.6
+P_s_a['s0']['a2']['sf'] = 0.4
+
+P_s_a['sg'] = {}
+P_s_a['sg']['l'] = {}
+P_s_a['sg']['l']['sl'] = 1
+P_s_a['sf'] = {}
+P_s_a['sf']['l'] = {}
+P_s_a['sf']['l']['sl'] = 1
+P_s_a['sl'] = {}
+P_s_a['sl']['l'] = {}
+P_s_a['sl']['l']['sl'] = 1
+
+G = nx.DiGraph()
+for state in P_s_a:
+    G.add_node(state, action=list(P_s_a[state].keys()))
+    for action in P_s_a[state]:
+        G.add_edge(state, (state, )+(action,))
+        for next_state in P_s_a[state][action]:
+            assert next_state in P_s_a
+            G.add_edge((state,)+(action,), next_state, action=action, prob=P_s_a[state][action][next_state])
+
+
+                     
+# create transition function 
+def transition_prob(s_a_s):
+    state = s_a_s[0]
+    action = s_a_s[1]
+    next_state = s_a_s[2]
+    
+    # unreachable state
+    if (action not in P_s_a[state]) or (next_state not in P_s_a[state][action]):
+        #print("trying to transit to an unreachable state")
+        return 0
+    
+    # failure state
+    if state == state_f:
+        #print('transit to failure state')
+        assert action == 'l' and (next_state == state_l)
+        return 1
+
+    # goal
+    if state == goal:
+        print("reach the goal and transition is {}".format(s_a_s))
+        assert action == 'l' and (next_state == state_l)
+        return 1
+    
+    # loop state
+    if state == state_l:
+        assert action == 'l' and (next_state == state_l)
+        return 1
+    
+    return P_s_a[state][action][next_state] 
+
+
+def reward(s_a):
+    state = s_a[0]
+    action = s_a[1]
+    
+    if state == state_f:
+        assert action == 'l', "should transit to loop state"
+        return 0
+    
+    if state == state_l:
+        assert action == 'l', "should transit to loop state"
+        return 0
+    
+    if state == goal:
+        assert action == 'l', "should transit to loop state"
+        return 0
+    
+    if action == 'a1':
+        return 1
+    
+    if action == 'a2':
+        return 5
+    
+
+def cost(s_a):
+    state = s_a[0]
+    action = s_a[1]
+    
+    if state == state_f:
+        assert action == 'l', "should transit to loop state"
+        return 0
+    
+    if state == state_l:
+        assert action == 'l', "should transit to loop state"
+        return 0
+    
+    if state == goal:
+        assert action == 'l', "should transit to loop state"
+        return 0
+    
+    # compute cost
+    C = 0
+    for next_state in P_s_a[state][action]:
+        if next_state == state_f:
+            C += P_s_a[state][action][next_state]
+    
+    assert C <= (1+1e-9)
+    
+    return C
+
+
+
+model = gp.Model('LP_CMDP')
+
+#indics for variables
+indices = []
+
+start_time = time.time()
+for state in P_s_a:
+    # if state == state_l:
+    #     continue
+    for action in P_s_a[state]:
+        indices.append((state,) + (action,))
+
+
+# add Non-negative continuous variables that lies between 0 and 1        
+rho = model.addVars(indices, lb=0,  vtype=GRB.CONTINUOUS,  name='rho')
+model.addConstrs((rho[s_a] >= 0.0 for s_a in indices), name='non-negative-ctrs')
+
+# add constraints 
+# cost constraints
+C = 0
+for s_a in indices:
+    # if cost(s_a)>0:
+    #     print("s_a {} cost is {}".format(s_a, cost(s_a)))
+    C += rho[s_a] * cost(s_a)
+cost_ctrs =  model.addConstr(C <= threshold, name='cost_ctrs')    
+
+print("start to add equality constraint")
+for state in P_s_a: 
+    if state == state_l:
+        continue
+    lhs = 0
+    rhs = 0
+    for action in P_s_a[state]:
+        #print(rho[(state,)+(action,)].x)
+        lhs += rho[(state,)+(action,)]
+        
+    # for s_a in indices:
+    #     s_a_s = s_a + state
+    #     rhs += rho[s_a] * transition_prob(s_a_s)
+
+    #s_a_nodes = []
+    for s_a in G.predecessors(state):
+        #assert len(s_a) == 6
+        #if s_a not in s_a_nodes:
+        #s_a_nodes.append(s_a)
+        s_a_s = s_a + (state,)
+        rhs += rho[s_a] * transition_prob(s_a_s)
+        
+    
+    if state == state_init:
+        print('initial state delta function')
+        rhs += 1
+    
+    model.addConstr(lhs == rhs, name = str(state))
+    model.update()
+    #print("equality constraint for state {}".format(state))
+
+
+    
+obj = 0
+for s_a in indices:
+    obj += rho[s_a] * reward(s_a)
+    
+model.setObjective(obj, GRB.MAXIMIZE)
+model.Params.FeasibilityTol = 1e-9    
+print("--- %s seconds ---" % (time.time() - start_time))
+
+model.optimize()
+
+print("objective value is {}".format(obj.getValue()))        
+# Saving the objects:
+       
+
+
+
+y = {}
+lambda_1 = 0
+lambda_sa = {}
+nu = {}
+C_sa_pri = {}
+C_sa_sec = {} 
+print("start to initialize")
+for state in P_s_a:
+    if state != state_l:
+        nu[state] = 10
+    for action in P_s_a[state]:
+        y[(state,)+(action,)] = 0
+        lambda_sa[(state,)+(action,)]  = 0
+        C_sa_pri[(state,)+(action,)] = reward((state,)+(action,))
+        assert C_sa_pri[(state,)+(action,)] >= 0
+        C_sa_sec[(state,)+(action,)] = cost((state,)+(action,))
+
+
+# update step size
+ay = 0.012
+a_lambda = 0.001
+a_nu = 0.001
+
+
+y_current_step = copy.deepcopy(y)
+lambda_1_current_step = lambda_1
+lambda_sa_current_step = copy.deepcopy(lambda_sa)
+nu_current_step = copy.deepcopy(nu)
+C_current_step = np.dot(np.array(list(y_current_step.values())), np.array(list(C_sa_pri.values())))
+
+
+y_last_step = copy.deepcopy(y)
+C_last_step = np.dot(np.array(list(y_last_step.values())), np.array(list(C_sa_pri.values())))-1
+lambda_1_last_step = lambda_1
+lambda_sa_last_step = copy.deepcopy(lambda_sa)
+nu_last_step = copy.deepcopy(nu)
+#Lagrangian_last_step = Lagrangian_current_step - 0.01
+
+
+
+obj_traces = [C_current_step]
+obj_mean = []
+y_traces = [y_current_step]
+y_mean = []
+#Lagrangian_traces = [Lagrangian_current_step]
+print("start to do iterations")
+for j in range(1, 8000):
+    print("iteration {}".format(j))
+    ay = ay/1.0
+    a_lambda = ay
+    a_nu = ay
+    #Lagrangian_last_step = Lagrangian_current_step
+    for s_a in y:
+        # dy
+        state = s_a[0]
+        last_term_dy = 0
+        for state_next in P_s_a[state][s_a[-1]]:
+            if state_next != state_l:
+                last_term_dy += P_s_a[state][s_a[-1]][state_next]*nu_last_step[state_next]
+        
+        if state != state_l:
+            dy = C_sa_pri[s_a] + lambda_1_last_step*C_sa_sec[s_a] - \
+                lambda_sa_last_step[s_a] + nu_last_step[state] - last_term_dy
+        else:
+            dy = C_sa_pri[s_a] + lambda_1_last_step*C_sa_sec[s_a] - lambda_sa_last_step[s_a] - last_term_dy
+        # update y
+        y_current_step[s_a] = y_last_step[s_a] - ay*dy
+        #y_current_step[s_a] = max(y_current_step[s_a], 0)
+        # d lambda_sa
+        d_lambda_sa = -y_last_step[s_a]
+        # update lambda_sa
+        lambda_sa_current_step[s_a] = lambda_sa_last_step[s_a] + a_lambda*d_lambda_sa
+        if lambda_sa_current_step[s_a] < 0:
+            lambda_sa_current_step[s_a] = 0
+        
+    y_traces.append(y_current_step)
+    # d lambda1
+    d_lambda1 = np.dot(np.array(list(y_last_step.values())), np.array(list(C_sa_sec.values()))) - threshold
+    # update lambda1
+    lambda_1_current_step = lambda_1_last_step + a_lambda*d_lambda1
+    if lambda_1_current_step < 0:
+        lambda_1_current_step = 0
+        
+    for state in P_s_a:
+        # d nu
+        if state != state_l:
+            # first term
+            d_nu_first_term = 0
+            for action in P_s_a[state]:
+                d_nu_first_term += y_last_step[(state,)+(action,)]
+            
+            # indicator
+            initial_state = 0
+            if state == state_init:
+                initial_state = 1
+            
+            # last term 
+            d_nu_last_term = 0
+            for s_a in G.predecessors(state):
+                #assert len(s_a) == 6
+                #if s_a not in s_a_nodes:
+                #s_a_nodes.append(s_a)
+                s_a_s = s_a + (state,)
+                d_nu_last_term += y_last_step[s_a] * transition_prob(s_a_s)
+                
+            d_nu = d_nu_first_term - initial_state - d_nu_last_term
+            
+            # update nu
+            nu_current_step[state] = nu_last_step[state] + a_nu*d_nu
+    
+    C_current_step = np.dot(np.array(list(y_current_step.values())), np.array(list(C_sa_pri.values())))
+    obj_traces.append(C_current_step)
+    
+    # update last step information
+    #y_last_step = copy.deepcopy(y_current_step)
+    y_last_step = dict(zip(y_last_step.keys(), y_current_step.values()))
+    #lambda_sa_last_step = copy.deepcopy(lambda_sa_current_step)
+    lambda_sa_last_step = dict(zip(lambda_sa_last_step.keys(), lambda_sa_current_step.values()))
+    
+    lambda_1_last_step = lambda_1_current_step
+    nu_last_step = dict(zip(nu_last_step.keys(), nu_current_step.values()))
+    #nu_last_step = copy.deepcopy(nu_current_step)
+
+# post processing
+for i in range(len(obj_traces)):
+    obj_mean.append(np.mean(obj_traces[0:i+1]))
+    
+print("optimal value is {}".format(obj_mean[-1]))       
+plt.plot(obj_mean)
+    
--- 2-LP-gradient_descent_test.py
+++ 2-LP-gradient_descent_test.py
@@ -14,12 +14,12 @@ y = m.addVar(name="y")
 m.setObjective(-(x + 2*y) , GRB.MINIMIZE)
 
 #Add constraints
-#m.addConstr(2*x + y  <= 20, "c1")
-#m.addConstr(-4*x + 5*y  <= 10, "c2")
-#m.addConstr(-x + 2*y >= -2, "c3")
-#m.addConstr(-x + 5*y == 15, "c4")
-m.addConstr(x  <= 0, "c5")
-m.addConstr(y  <= 0, "c6")
+m.addConstr(2*x + y  <= 20, "c1")
+m.addConstr(-4*x + 5*y  <= 10, "c2")
+m.addConstr(-x + 2*y >= -2, "c3")
+m.addConstr(-x + 5*y == 15, "c4")
+m.addConstr(x  >= 0, "c5")
+m.addConstr(y  >= 0, "c6")
 
 # Optimize model
 m.optimize()
@@ -36,13 +36,14 @@ x_currrent_step = 0
 y_currrent_step = 0
 u1_currrent_step, u2_currrent_step, u3_currrent_step, u4_currrent_step, u5_currrent_step = 0, 0, 0, 0, 0
 v1_currrent_step = 0
-#L_current_step = -(x_currrent_step + 2*y_currrent_step) + u1_currrent_step*(2*x_currrent_step+y_currrent_step-20)+\
-#    u2_currrent_step*(-4*x_currrent_step+5*y_currrent_step-10) +\
- #       u3_currrent_step*(x_currrent_step-2*y_currrent_step-2) + \
-#            u4_currrent_step*(-x_currrent_step) + \
-#                u5_currrent_step*(-y_currrent_step) + \
-#                  v1_currrent_step*(-x_currrent_step+5*y_currrent_step-15)
-L_current_step = -(x_currrent_step + 2*y_currrent_step) + u4_currrent_step*(x_currrent_step)+u5_currrent_step*(y_currrent_step)
+L_current_step = -(x_currrent_step + 2*y_currrent_step) + u1_currrent_step*(2*x_currrent_step+y_currrent_step-20)+\
+    u2_currrent_step*(-4*x_currrent_step+5*y_currrent_step-10) +\
+       u3_currrent_step*(x_currrent_step-2*y_currrent_step-2) + \
+            u4_currrent_step*(-x_currrent_step) + \
+                u5_currrent_step*(-y_currrent_step) + \
+                  v1_currrent_step*(-x_currrent_step+5*y_currrent_step-15)
+
+#L_current_step = -(x_currrent_step + 2*y_currrent_step) + u4_currrent_step*(x_currrent_step)+u5_currrent_step*(y_currrent_step)
 
 x_last_step = 2
 y_last_step = 2
@@ -54,21 +55,20 @@ ax = 0.1
 L_traces = [L_current_step]
 traces = [x_currrent_step+2*y_currrent_step]
 while (abs(L_current_step - L_last_step) >0.001):
-    ax=ax/1.1
+    ax=ax/1
     ay = ax
     au = ax
     av = ax 
     L_last_step = L_current_step
-    #dx = -1 + 2*u1_last_step -4*u2_last_step + u3_last_step -u4_last_step - v1_last_step
-    dx = -1 + u4_last_step
-    #dy = -2 + u1_last_step +5*u2_last_step - 2*u3_last_step - u5_last_step +5*v1_last_step
-    dy = -2 + u5_last_step
-#    du1 = 2*x_last_step + y_last_step -20
-#    du2 = -4*x_last_step + 5*y_last_step -10
-#    du3 = x_last_step -2*y_last_step -2
-    du4 = x_last_step
-    du5 = y_last_step
-#    dv1 = -x_last_step + 5*y_last_step -15
+    dx = -1 + 2*u1_last_step -4*u2_last_step + u3_last_step -u4_last_step - v1_last_step
+    
+    dy = -2 + u1_last_step +5*u2_last_step - 2*u3_last_step - u5_last_step +5*v1_last_step
+    du1 = 2*x_last_step + y_last_step -20
+    du2 = -4*x_last_step + 5*y_last_step -10
+    du3 = x_last_step -2*y_last_step -2
+    du4 = -x_last_step
+    du5 = -y_last_step
+    dv1 = -x_last_step + 5*y_last_step -15
     
     x_currrent_step = x_last_step - ax*dx
     #x_currrent_step = min(x_currrent_step, 0)
